595b9258afd1bf156fb4307e926918a2cc48e316
Muhammad Ali <89317342+SeAliijaz@users.noreply.github.com>
2023-08-31 08:15:47 +0500

    README.MD

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:flutter_ad_player_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const UnityAdsExampleApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
